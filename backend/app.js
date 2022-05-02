3ba0b04bd0c2e150b20a84c5510559085918bb3d
Artem Pavlov <Zireaeell@yandex.ru>
2022-05-02 23:06:36 +0300

    Исправление

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('./middlewares/cors');
 const { errors } = require('celebrate');
 
 const { createUser, login } = require('./controllers/users');
