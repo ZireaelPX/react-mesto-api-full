70d7eca980c29b61c1e021e967e1d052430ae269
Artem Pavlov <Zireaeell@yandex.ru>
2022-05-03 12:30:57 +0300

    auth

--- frontend/src/utils/mestoAuth.js
+++ frontend/src/utils/mestoAuth.js
@@ -4,7 +4,8 @@ export function register(email, password) {
     return fetch(`${BASE_URL}/signup`, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify({ email, password }),
     }).then(checkResponse);
@@ -14,16 +15,19 @@ export function login(email, password) {
     return fetch(`${BASE_URL}/signin`, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify({ email, password }),
     }).then(checkResponse);
 }
 
+
 export function getToken(jwt) {
     return fetch(`${BASE_URL}/users/me`, {
         method: "GET",
         headers: {
+            'Accept': 'application/json',
             "Content-Type": "application/json",
             Authorization: `Bearer ${jwt}`,
         },
